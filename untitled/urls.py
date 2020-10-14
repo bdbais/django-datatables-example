6e0e1157d27c284c97492a8fb9cdc54ebbcb5af0
bdbais <bais@bais.info>
2020-10-14 15:23:22 +0200

    Update urls.py
    django.core.exceptions.ImproperlyConfigured: Passing a 3-tuple to include() is not supported. Pass a 2-tuple containing the list of patterns and app_name, and provide the namespace argument to include() instead.
    
    https://timonweb.com/wagtail/solving-djangocoreexceptionsimproperlyconfigured-passing-a-3-tuple-to-include-error-after-djangowagtail-20-upgrade/

--- untitled/urls.py
+++ untitled/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
    
     url(r'/home/ajax/', 'test_app.views.process_ajax'),
     url(r'^','test_app.views.home')
